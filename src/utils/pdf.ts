a71ba9e8b2140e3972f1672867d48c71bc066fd4
stonek <amit-yurist@mail.ru>
2025-10-10 13:58:58 +0300

    feat: Добавление типов для обработки текста в PDF, включая TextItem, TextMarkedContent, TextStyle и TextContent, для улучшения работы с текстовым содержимым в PDF-документах.

--- src/utils/pdf.ts
+++ src/utils/pdf.ts
@@ -1,6 +1,97 @@
 // import { type PDFDocumentProxy, getDocument } from 'pdfjs-dist';
 // import 'pdfjs-dist/build/pdf.worker.mjs';
-import type { TextContent, TextItem } from 'pdfjs-dist/types/src/display/api';
+// import type { TextContent, TextItem } from 'pdfjs-dist/types/src/display/api';
+
+// Типы скопированы из библиотеки pdfjs-dist для ускорения и простоты, подумать над другим решением для более правильного подхода
+
+/**
+ * Page text content part.
+ */
+export type TextItem = {
+    /**
+     * - Text content.
+     */
+    str: string;
+    /**
+     * - Text direction: 'ttb', 'ltr' or 'rtl'.
+     */
+    dir: string;
+    /**
+     * - Transformation matrix.
+     */
+    transform: Array<any>;
+    /**
+     * - Width in device space.
+     */
+    width: number;
+    /**
+     * - Height in device space.
+     */
+    height: number;
+    /**
+     * - Font name used by PDF.js for converted font.
+     */
+    fontName: string;
+    /**
+     * - Indicating if the text content is followed by a
+     * line-break.
+     */
+    hasEOL: boolean;
+};
+/**
+ * Page text marked content part.
+ */
+export type TextMarkedContent = {
+    /**
+     * - Either 'beginMarkedContent',
+     * 'beginMarkedContentProps', or 'endMarkedContent'.
+     */
+    type: string;
+    /**
+     * - The marked content identifier. Only used for type
+     * 'beginMarkedContentProps'.
+     */
+    id: string;
+};
+/**
+ * Text style.
+ */
+export type TextStyle = {
+    /**
+     * - Font ascent.
+     */
+    ascent: number;
+    /**
+     * - Font descent.
+     */
+    descent: number;
+    /**
+     * - Whether or not the text is in vertical mode.
+     */
+    vertical: boolean;
+    /**
+     * - The possible font family.
+     */
+    fontFamily: string;
+};
+/**
+ * Page text content.
+ */
+export type TextContent = {
+    /**
+     * - Array of
+     * {@link TextItem } and {@link TextMarkedContent } objects. TextMarkedContent
+     * items are included when includeMarkedContent is true.
+     */
+    items: Array<TextItem | TextMarkedContent>;
+    /**
+     * - {@link TextStyle } objects,
+     * indexed by font name.
+     */
+    styles: {
+        [x: string]: TextStyle;
+    };
+};
 
 function isTextItem(item: TextContent['items'][number]): item is TextItem {
     return typeof (item as { str?: unknown }).str === 'string';
@@ -25,6 +116,7 @@ export class PdfProcessor {
         // const loadingTask = getDocument({ data: buffer });
         const pdfjsDistPath = 'pdfjs-dist/build/pdf.js';
         const { getDocument } = await import(pdfjsDistPath);
+
 
         const loadingTask = getDocument({ data: buffer });
         type PDFDocumentProxy = Awaited<typeof loadingTask.promise>;
